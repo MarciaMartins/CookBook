1cfa7ae84f32c3e189f6ab70ae24a28dd683ba1d
Marcia Martins <marciapmartins@gmail.com>
2017-06-29 00:13:31 -0300

    Usuário envia suas receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,29 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+      @recipe = Recipe.new(recipe_params)
+    # @recipe.title = params[:recipe][:title]
+    # @recipe.recipe_type = params[:recipe][:recipe_type]
+    # @recipe.cuisine = params[:recipe][:cuisine]
+    # @recipe.difficulty= params[:recipe][:difficulty]
+    # @recipe.cook_time= params[:recipe][:cook_time]
+    # @recipe.ingredients= params[:recipe][:ingredients]
+    # @recipe.method= params[:recipe][:method]
+    @recipe.save
+    redirect_to recipe_url(@recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title,
+                          :recipe_type,:cuisine,:difficulty,:cook_time,:ingredients, :method)
+  end
+
+
 end
