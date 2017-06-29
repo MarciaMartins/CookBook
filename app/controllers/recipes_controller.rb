98fca9b59c5711fe982b8857841dbfbfc2481490
Marcia Martins <marciapmartins@gmail.com>
2017-06-29 01:05:26 -0300

    Usuário usa cozinhas pré cadastradas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,26 +5,20 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @cuisines = Cuisine.all
   end
 
   def create
-      @recipe = Recipe.new(recipe_params)
-    # @recipe.title = params[:recipe][:title]
-    # @recipe.recipe_type = params[:recipe][:recipe_type]
-    # @recipe.cuisine = params[:recipe][:cuisine]
-    # @recipe.difficulty= params[:recipe][:difficulty]
-    # @recipe.cook_time= params[:recipe][:cook_time]
-    # @recipe.ingredients= params[:recipe][:ingredients]
-    # @recipe.method= params[:recipe][:method]
+    @recipe = Recipe.new(recipe_params)
     @recipe.save
-    redirect_to recipe_url(@recipe)
+    redirect_to recipe_url(@recipe.id)
   end
 
   private
 
   def recipe_params
     params.require(:recipe).permit(:title,
-                          :recipe_type,:cuisine,:difficulty,:cook_time,:ingredients, :method)
+                          :recipe_type,:cuisine_id,:difficulty,:cook_time,:ingredients, :method)
   end
 
 
